1bb67a2401585628872499fdf1938af22ae58e76
VitaliyBIletov <vitaxa17@yandex.ru>
2020-09-03 21:28:57 +0300

    Регистрация, авторизация, выход

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Вход в систему</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?
+// Страница регистрации нового пользователя
+
+// Соединямся с БД
+$link=mysqli_connect("localhost", "mysql", "mysql", "testtable");
+
+if(isset($_POST['submit']))
+{
+    $err = [];
+
+    // проверям логин
+    if(!preg_match("/^[a-zA-Z0-9]+$/",$_POST['login']))
+    {
+        $err[] = "Логин может состоять только из букв английского алфавита и цифр";
+    }
+
+    if(strlen($_POST['login']) < 3 or strlen($_POST['login']) > 30)
+    {
+        $err[] = "Логин должен быть не меньше 3-х символов и не больше 30";
+    }
+
+    // проверяем, не сущестует ли пользователя с таким именем
+    $query = mysqli_query($link, "SELECT user_id FROM users WHERE user_login='".mysqli_real_escape_string($link, $_POST['login'])."'");
+    if(mysqli_num_rows($query) > 0)
+    {
+        $err[] = "Пользователь с таким логином уже существует в базе данных";
+    }
+
+    // Если нет ошибок, то добавляем в БД нового пользователя
+    if(count($err) == 0)
+    {
+
+        $login = $_POST['login'];
+
+        // Убераем лишние пробелы и делаем двойное хеширование
+        $password = md5(md5(trim($_POST['password'])));
+
+        mysqli_query($link,"INSERT INTO users SET user_login='".$login."', user_password='".$password."'");
+        header("Location: index.php"); exit();
+    }
+    else
+    {
+        print "<b>При регистрации произошли следующие ошибки:</b><br>";
+        foreach($err AS $error)
+        {
+            print $error."<br>";
+        }
+    }
+}
+?>
+	<form class="box" method="post">
+		<h1>Aisor v.1.0</h1>
+		<input type="text" name="login" placeholder="Username">
+		<input type="password" name="password" placeholder="Password">
+		<input type="submit" name="submit" value="Login">
+	</form>
+</body>
+</html>
